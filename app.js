6219ea1d29910afd121473c3ea51f382dfc63f05
HY <homtele@gmail.com>
2021-11-28 19:52:21 +0800

    search restaurants

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+  res.render('index', { keyword, restaurants })
+})
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant })
