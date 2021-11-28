ec993db575794ba36de06bdd59f233ac2329e333
HY <homtele@gmail.com>
2021-11-28 22:45:45 +0800

    fix the problem of missing keyword

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
   if (restaurants.length === 0) {
-    res.render('not-found')
+    res.render('not-found', { keyword })
   } else {
     res.render('index', { keyword, restaurants })
   }
