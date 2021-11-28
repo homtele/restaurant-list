a1278f1596440cd0beef97148f597305755ef235
HY <homtele@gmail.com>
2021-11-28 19:28:03 +0800

    render restaurant.json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
@@ -8,7 +9,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 app.get('/restaurants/1', (req, res) => {
   res.render('show')
