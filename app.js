9b806dfe382c2053fe1882a4bcdda7cb8b756a8f
HY <homtele@gmail.com>
2021-11-28 22:29:07 +0800

    add not-found.handlebars

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-  res.render('index', { keyword, restaurants })
+  if (restaurants.length === 0) {
+    res.render('not-found')
+  } else {
+    res.render('index', { keyword, restaurants })
+  }
 })
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
