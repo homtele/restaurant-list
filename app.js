291cf18cb60557c01781235087aa8d473dc07da7
HY <homtele@gmail.com>
2021-12-06 01:19:20 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find().lean().then(restaurants => {
+    res.render('index', { restaurants })
+  }).catch(error => {
+    console.error(error)
+  })
 })
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-  if (restaurants.length === 0) {
-    res.render('not-found', { keyword })
-  } else {
-    res.render('index', { keyword, restaurants })
-  }
+  const keyword = req.query.keyword.toLowerCase()
+  Restaurant.find().lean().then(restaurants => {
+    restaurants = restaurants.filter(restaurant =>
+      restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+    )
+    if (restaurants.length === 0) {
+      res.render('not-found', { keyword })
+    } else {
+      res.render('index', { keyword, restaurants })
+    }
+  }).catch(error => {
+    console.error(error)
+  })
 })
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant })
+  Restaurant.findById(req.params.restaurant_id).lean().then(restaurant => {
+    res.render('show', { restaurant })
+  }).catch(error => {
+    console.error(error)
+  })
 })
 
 app.listen(port, () => {
