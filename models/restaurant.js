52116c9adc6cd9b3ed60a9fca31abc2d07d2f503
HY <homtele@gmail.com>
2021-12-06 16:39:02 +0800

    feat: add delete confirmation function

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: { type: String, required: true },
-  category: { type: String },
-  image: { type: String },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
   location: { type: String },
   phone: { type: String },
   google_map: { type: String },
-  rating: { type: String },
+  rating: { type: Number, required: true },
   description: { type: String }
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
