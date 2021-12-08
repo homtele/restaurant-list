9bd69a850812d18bd8f233e156c80a6af88b57e3
HY <homtele@gmail.com>
2021-12-08 17:02:53 +0800

    feat: add sort function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,9 @@
+document.querySelector('#sort').addEventListener('change', event => {
+  if (event.target.matches('select')) {
+    event.target.parentElement.parentElement.submit()
+  }
+})
+
 document.querySelector('#data-panel').addEventListener('click', event => {
   if (event.target.matches('.delete-btn')) {
     document.querySelector('#modal-body').textContent = `確定要刪除『${event.target.dataset.name}』嗎？`
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  Restaurant.find().lean().then(restaurants => {
+  Restaurant.find().lean().sort({ name: 'asc' }).then(restaurants => {
     res.render('index', { restaurants, length: restaurants.length })
   }).catch(error => {
     console.error(error)
@@ -12,15 +12,44 @@ router.get('/', (req, res) => {
 })
 
 router.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  Restaurant.find().lean().then(restaurants => {
-    restaurants = restaurants.filter(restaurant =>
-      restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-    )
-    res.render('index', { keyword, restaurants, length: restaurants.length })
-  }).catch(error => {
-    console.error(error)
-  })
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const sort = req.query.sort
+  switch (sort) {
+    case 'name_asc':
+      Restaurant.find().lean().sort({ name: 'asc' }).then(restaurants => {
+        restaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+        res.render('index', { keyword, restaurants, nameAsc: true })
+      }).catch(error => {
+        console.error(error)
+      })
+      break
+    case 'name_desc':
+      Restaurant.find().lean().sort({ name: 'desc' }).then(restaurants => {
+        restaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+        res.render('index', { keyword, restaurants, nameDesc: true })
+      }).catch(error => {
+        console.error(error)
+      })
+      break
+    case 'category_asc':
+      Restaurant.find().lean().sort({ category: 'asc' }).then(restaurants => {
+        restaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+        res.render('index', { keyword, restaurants, categoryAsc: true })
+      }).catch(error => {
+        console.error(error)
+      })
+      break
+    case 'rating_desc':
+      Restaurant.find().lean().sort({ rating: 'desc' }).then(restaurants => {
+        restaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword))
+        res.render('index', { keyword, restaurants, ratingDesc: true })
+      }).catch(error => {
+        console.error(error)
+      })
+      break
+    default:
+      res.redirect('/')
+  }
 })
 
 module.exports = router
